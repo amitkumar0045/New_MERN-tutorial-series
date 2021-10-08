96655005bf92ba89634ee76de283ab0c9d41fc9d
amit.kumar@gupshup.io <amit.kumar@gupshup.io>
2021-10-08 17:43:20 +0530

    initialized frantend & backend

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+
+const express = require('express')
+const notes = require('./data/notes') //dummy  data
+const dotenv = require('dotenv')
+
+const app = express();
+dotenv.config();
+
+
+
+app.get("/", (req, res) => {
+    res.send("API is RUNNING....")
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+
+
+app.get('/api/notes/:id', (req, res) => {
+    const note = notes.find((n) => n._id === req.params.id);
+    res.send(note);
+})
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(
+        `Server running on port ${PORT}`
+    )
+);
+
