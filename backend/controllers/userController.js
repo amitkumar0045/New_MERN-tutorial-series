2f22d5056353df7eb1284adaf32e29277ec1a789
amit.kumar@gupshup.io <amit.kumar@gupshup.io>
2021-10-12 17:04:11 +0530

    bkend=> added jwt_token to user register & login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
-// const generateToken = require('../utils/generateToken');
+const generateToken = require('../utils/generateToken');
 
 //----><---- register new USER
 const registerUser = asyncHandler(async (req, res) => {
@@ -29,7 +29,7 @@ const registerUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            // token: generateToken(user._id) //jwt token
+            token: generateToken(user._id) //jwt token
             // password: user.password,
         });
     } else {
@@ -56,7 +56,7 @@ const authUser = asyncHandler(async (req, res) => {
             email: user.email,
             isAdmin: user.isAdmin,
             pic: user.pic,
-            //token: generateToken(user._id) //jwt token
+            token: generateToken(user._id) //jwt token
         });
     } else {
         res.status(401)
