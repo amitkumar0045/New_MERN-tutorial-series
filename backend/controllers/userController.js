8deeb5b2cf3e29b134d409a46a5e730c3dd3cccc
amit.kumar@gupshup.io <amit.kumar@gupshup.io>
2021-10-12 14:53:16 +0530

    password encrypt functionally added to userModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,45 @@
-const registerUser = async (req, res) => {
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+//----><---- register new USER
+const registerUser = asyncHandler(async (req, res) => {
+    // const registerUser = async (req, res) => {  // >> without asyncHandler
+
     const { name, email, password, pic } = req.body;
 
-    res.json({
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400)
+        throw new Error("User Allredy Exists")
+    }
+
+    const user = await User.create({
         name,
         email,
-    })
-};
+        password,
+        pic
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            pic: user.pic,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            // password: user.password,
+        });
+    } else {
+        res.status(400)
+        throw new Error('!Error Occured ! User not Found !')
+    }
+    // res.json({
+    //     name,
+    //     email,
+    // });
+});
+
+
 
 module.exports = { registerUser }
