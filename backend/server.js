75857a07d3b15f5d5427ff1a6845a4b076052d72
amit.kumar@gupshup.io <amit.kumar@gupshup.io>
2021-10-12 13:16:41 +0530

    bkend=> our-1st route added i.e> api.users for registering new users

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,40 @@
-
 const express = require('express')
-const notes = require('./data/notes') //dummy  data
-const dotenv = require('dotenv');
+const notes = require('./data/notes') //dummy data for check only
+const dotenv = require("dotenv");//contains secrt info like api-keys
 const connectDB = require('./config/db');
 
+const userRoute = require('./routes/userRoute')
+
+
+
 const app = express();
+app.use(express.json());
 dotenv.config();
 connectDB();
 
+//\\----
+// app.get("/", (req, res) => {
+//     res.send("API is RUNNING....")
+// })
+
+// app.get('/api/notes', (req, res) => {
+//     res.json(notes)
+// })
+
+// app.get('/api/notes/:id', (req, res) => {
+//     const note = notes.find((n) => n._id === req.params.id);
+//     res.send(note);
+// })
+//\\----
+
+app.use('/api/users', userRoute);  // using Routes
 
 
-app.get("/", (req, res) => {
-    res.send("API is RUNNING....")
-})
-
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-})
-
-
-
-app.get('/api/notes/:id', (req, res) => {
-    const note = notes.find((n) => n._id === req.params.id);
-    res.send(note);
-})
-
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
     console.log(
-        `Server running on port ${PORT}`
+        `Server Started on port ${PORT}`
     )
 );
-
